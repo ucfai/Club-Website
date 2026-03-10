efa32a28d99a65efaf5b4d24805b231e58206a38
GauravKano <gaurav.kanoujia2006@gmail.com>
2026-03-10 11:26:49 -0400

    Create SideNav for mobile view

---
+++ src/components/MobileNavbarButton.tsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const MobileNavbarButton = ({ currentPage }: { currentPage: string }) => {
+  const [sidenavOpen, setSidenavOpen] = useState<boolean>(false);
+
+  const openSidenav = () => {
+    setSidenavOpen(true);
+  };
+
+  const closeSidenav = () => {
+    setSidenavOpen(false);
+
+    if ((window as any).resumeParticles) {
+      (window as any).resumeParticles();
+    }
+  };
+
+  return (
+    <>
+      <button
+        className="cursor-pointer no-particles p-1.5 w-10 h-10 flex justify-center items-center bg-white rounded-2xl"
+        onClick={openSidenav}
+      >
+        <FaBars className="w-4.5 h-4.5 text-black my-auto" />
+      </button>
+
+      {sidenavOpen && (
+        <>
+          <div
+            className="0_5xl:hidden fixed top-0 left-0 w-screen h-dvh z-30 opacity-80 bg-black"
+            onClick={closeSidenav}
+          />
+
+          <div className="no-particles 0_5xl:hidden fixed flex flex-col justify-center items-center gap-10 px-7 py-10 top-0 right-0 z-50 max-w-80 w-3/4 h-dvh overflow-auto p-4 bg-[#0f0f0f] shadow-[0_2px_10px_#FFFFFF] font-inria-sans text-xl font-semibold tracking-wide">
+            <button
+              className="no-particles fixed top-6.5 right-6.5 cursor-pointer"
+              aria-label="Close Button"
+              onClick={closeSidenav}
+            >
+              <FaTimes className="w-7 h-7 text-white my-auto" />
+            </button>
+
+            <a
+              href={currentPage !== "Home" ? "/" : "#"}
+              onClick={(e) => {
+                if (currentPage === "Home") {
+                  e.preventDefault();
+                  closeSidenav();
+                }
+              }}
+              className={
+                currentPage === "Home" ? "text-[#F8D03F]" : "text-white"
+              }
+            >
+              Home
+            </a>
+            <a
+              href={currentPage !== "About" ? "/about" : "#"}
+              onClick={(e) => {
+                if (currentPage === "About") {
+                  e.preventDefault();
+                  closeSidenav();
+                }
+              }}
+              className={
+                currentPage === "About" ? "text-[#F8D03F]" : "text-white"
+              }
+            >
+              About
+            </a>
+            <a
+              href={currentPage !== "Events" ? "/events" : "#"}
+              onClick={(e) => {
+                if (currentPage === "Events") {
+                  e.preventDefault();
+                  closeSidenav();
+                }
+              }}
+              className={
+                currentPage === "Events" ? "text-[#F8D03F]" : "text-white"
+              }
+            >
+              Events
+            </a>
+            <a
+              href={currentPage !== "Projects" ? "/projects" : "#"}
+              onClick={(e) => {
+                if (currentPage === "Projects") {
+                  e.preventDefault();
+                  closeSidenav();
+                }
+              }}
+              className={
+                currentPage === "Projects" ? "text-[#F8D03F]" : "text-white"
+              }
+            >
+              Projects
+            </a>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default MobileNavbarButton;
